e6017ee3d748888cb5ce7a14674ff8ea7faf30c8
yoctoguys <pokyyocto@gmail.com>
2024-02-07 16:48:26 +0530

    Update JSON data from Excel file

--- run.sh
+++ run.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
-node main_json.js
-git diff > diff.log
-git add .
-git commit -m "Update JSON data from Excel file"
-# git push -u origin main
-git log > log.log
 
+# Run Node.js script for JSON processing
+node main_json.js
+
+# Export Git diff to a file and append to diff.log
+git diff >> diff.log
+
+# Add all changes to the Git staging area
+git add .
+
+# Commit changes with a message
+git commit -m "Update JSON data from Excel file"
+
+# git push -u origin main
+
+# Export Git log to a file and append to log.log
+git log >> log.log
